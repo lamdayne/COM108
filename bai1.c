1fe6111fa397d3fb244058be3641d509683b52c4
Nguyễn Hoàng Phương Lâm <142385587+lamdayne@users.noreply.github.com>
2024-09-18 22:52:08 +0700

    Add files via upload

---
+++ bai1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Nhap hai so a va b: ");
+    scanf("%d %d", &a, &b);
+    printf("Tong hai so a va b la: %d\n", a + b);
+    printf("Hieu hai so a va b la: %d\n", a - b);
+    return 0;
+}
