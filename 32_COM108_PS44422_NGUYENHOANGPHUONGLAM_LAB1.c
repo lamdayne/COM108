d33ae6c3fc7d2bc5817c83ccce3af81534d45194
Nguyễn Hoàng Phương Lâm <142385587+lamdayne@users.noreply.github.com>
2024-09-18 22:51:40 +0700

    Add files via upload

---
+++ 32_COM108_PS44422_NGUYENHOANGPHUONGLAM_LAB1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("Tui ten la: Lâm\n");
+    printf("Ma So Sinh Vien : PS44422\n");
+    int tuoi;
+    printf("Nhap vao tuoi cua ban: ");
+    scanf("%d", &tuoi);
+    printf("Nam nay ban %d tuoi", tuoi);
+    return 0;
+}
