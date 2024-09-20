55518c66fb0b00d0eb6a662a952cea9240097507
Nguyễn Hoàng Phương Lâm <142385587+lamdayne@users.noreply.github.com>
2024-09-20 19:04:43 +0700

    Update bai4_lab3.c

--- bai4_lab3.c
+++ bai4_lab3.c
@@ -77,7 +77,33 @@ int main(){
             break;
         }
         case 4: {
-            printf("Đang sửa");
+            int sodien, tien;
+            printf("Nhap so dien : ");
+            scanf("%d", &sodien);
+            if(sodien <= 50){
+                tien = sodien * 1678;
+                printf("Tien dien la : %d VND", tien);
+            }
+            else if(sodien <= 100){
+                tien = (50 * 1678) + (sodien - 50) * 1734;
+                    printf("Tien dien la : %d VND", tien);
+            }
+            else if (sodien <= 200){
+                tien = (50 *1678) + (50 * 1734) + (sodien - 100) * 2014;
+                printf("Tien dien la : %d VND", tien);
+            }
+            else if(sodien <= 300){
+                tien = (50 * 1678) + (50 * 1734) + (100 * 2014) + (sodien - 200) * 2536;
+                printf("Tien dien la : %d VND", tien);
+            }
+            else if(sodien <= 400){
+                tien = (50 * 1678) + (50 * 1734) + (100 * 2014) + (100 * 2536) + (sodien - 300) * 2834;
+                printf("Tien dien la : %d VND", tien);
+            }
+            else{
+                tien = (50 * 1678) + (50 * 1734) + (100 * 2014) + (100 * 2536) + (100 * 2834) + (sodien - 400) * 2927;
+                printf("Tien dien la : %d VND", tien);
+            }
             break;
         }
         default:
