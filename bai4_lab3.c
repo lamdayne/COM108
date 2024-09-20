140d294f8d78417f8674e7633553a15da542a0c3
Nguyễn Hoàng Phương Lâm <142385587+lamdayne@users.noreply.github.com>
2024-09-20 17:36:36 +0700

    Update bai4_lab3.c

--- bai4_lab3.c
+++ bai4_lab3.c
@@ -77,27 +77,7 @@ int main(){
             break;
         }
         case 4: {
-            float sodien;
-            printf("Vui long nhap so dien : ");
-            scanf("%f", &sodien);
-            if (sodien > 0 && sodien <= 50){
-                printf("Tien dien la : %.2f VND", sodien * 1678);
-            }
-            else if (sodien > 50 && sodien <= 100){
-                printf("Tien dien la : %.2f VND", sodien * 1734);
-            }
-            else if (sodien > 100 && sodien <= 200){
-                printf("Tien dien la : %.2f VND", sodien * 2014);
-            }
-            else if (sodien > 200 && sodien <= 300){
-                printf("Tien dien la : %.2f VND", sodien * 2536);
-            }
-            else if (sodien > 300 && sodien <= 400){
-                printf("Tien dien la : %.2f VND", sodien * 2834);
-            }
-            else{
-                printf("Tien dien la : %.2f VND", sodien * 2927);
-            }
+            printf("Đang sửa");
             break;
         }
         default:
