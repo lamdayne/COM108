c51b0afc46c070241d2bf9330d37e6f8cf68c1f9
Nguyễn Hoàng Phương Lâm <142385587+lamdayne@users.noreply.github.com>
2024-09-25 19:47:28 +0700

    Update bai4_lab4.c

--- bai4_lab4.c
+++ bai4_lab4.c
@@ -49,13 +49,18 @@ int main(){
             break;
         case 3:
             int n;
-            printf("Nhap vao so can kiem tra : ");
-            scanf("%d", &n);
-            float x = sqrt(n);
-            if (x == (int)x){
-                printf("%d la so chinh phuong\n", n);
+            do {
+                printf("Nhap vao mot so nguyen duong: ");
+                scanf("%d", &n);
+                if (n < 0) {
+                    printf("So khong hop le, vui long nhap lai!\n");
+                }
+            }while (n < 0);
+            int x = (int)sqrt(n);
+            if (x * x == n) {
+                printf("%d la so chinh phuong.\n", n);
             }else{
-                printf("%d khong la chinh phuong\n", n);
+                printf("%d khong phai la so chinh phuong.\n", n);
             }
             break;
         default:
